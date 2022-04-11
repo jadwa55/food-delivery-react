5baaa1d6471fd7ffb3b194a60118f93178a2aaf0
jadwa55 <75550231+jadwa55@users.noreply.github.com>
2022-04-11 11:27:50 +0000

    Header styles

--- fooddelivery/src/App.js
+++ fooddelivery/src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
 
+import Header from "./Components/Header";
+
 function App() {
   return (
     <div className="App">
+      {/* Header Senction */}
+      <Header />
+
+      {/* Main Container */}
+
+      {/* Bottom Menu */}
     </div>
   );
 }
